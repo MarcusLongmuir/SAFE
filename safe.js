ca8172d6b0f27f59fbda8e2ac3e71b0e944f0cec
Marcus Longmuir <marcuslongmuir@me.com>
2014-11-11 11:51:32 +0000

    Removed the maximum duration of a tap

--- safe.js
+++ safe.js
@@ -2313,7 +2313,6 @@
         var delta = Math.max(xDelta, yDelta);
 
         return (
-            e.timeStamp - startEvent.timeStamp < $.tap.TIME_DELTA &&
             delta < $.tap.POSITION_DELTA &&
             (!startEvent.touches || TOUCH_VALUES.count === 1) &&
             Tap.isTracking
@@ -2546,7 +2545,6 @@
     // Configurable options
     $.tap = {
         POSITION_DELTA: 10, // Max distance between touchstart and touchend to be considered a tap
-        TIME_DELTA: 400, // Max duration between touchstart and touchend to be considered a tap
         LEFT_BUTTON_ONLY: true // Only accept left mouse button actions
     };
 
